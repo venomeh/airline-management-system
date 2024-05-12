fdd4f70d1eac8c1549d64a2cc051fea6385932ec
Muhammad Taha <muhammadtaha2723@gmail.com>
2024-05-12 12:02:50 +0500

    removed unnecessary libraries

--- AirlineServices.cs
+++ AirlineServices.cs
@@ -1,12 +1,4 @@
-﻿using Oracle.ManagedDataAccess.Client;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace DATABASE_PROJECT
