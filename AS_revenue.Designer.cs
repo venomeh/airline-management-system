4e6cf356faaa3dc6c517d6d9b62ec35a60dbf945
Muhammad Abdullah Mahmood <muhammadabdullahmahmood@gmail.com>
2024-05-13 00:41:00 +0500

    created revenue form

---
+++ AS_revenue.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace DATABASE_PROJECT
+{
+    partial class AS_revenue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.aircraftId_label = new System.Windows.Forms.Label();
+            this.flightID_label = new System.Windows.Forms.Label();
+            this.maintainance_label = new System.Windows.Forms.Label();
+            this.statusOfFlight_label = new System.Windows.Forms.Label();
+            this.revenueGenerated_label = new System.Windows.Forms.Label();
+            this.cost_label = new System.Windows.Forms.Label();
+            this.airline_comboBox = new System.Windows.Forms.ComboBox();
+            this.maintainance_comboBox = new System.Windows.Forms.ComboBox();
+            this.flightid_comboBox = new System.Windows.Forms.ComboBox();
+            this.revenueGeneratedOut_label = new System.Windows.Forms.Label();
+            this.StatusOfFlightOut_label = new System.Windows.Forms.Label();
+            this.costOut_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // aircraftId_label
+            // 
+            this.aircraftId_label.AutoSize = true;
+            this.aircraftId_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aircraftId_label.Location = new System.Drawing.Point(124, 48);
+            this.aircraftId_label.Name = "aircraftId_label";
+            this.aircraftId_label.Size = new System.Drawing.Size(106, 25);
+            this.aircraftId_label.TabIndex = 1;
+            this.aircraftId_label.Text = "Aircraft ID";
+            // 
+            // flightID_label
+            // 
+            this.flightID_label.AutoSize = true;
+            this.flightID_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flightID_label.Location = new System.Drawing.Point(124, 105);
+            this.flightID_label.Name = "flightID_label";
+            this.flightID_label.Size = new System.Drawing.Size(91, 25);
+            this.flightID_label.TabIndex = 2;
+            this.flightID_label.Text = "Flight ID";
+            // 
+            // maintainance_label
+            // 
+            this.maintainance_label.AutoSize = true;
+            this.maintainance_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maintainance_label.Location = new System.Drawing.Point(124, 159);
+            this.maintainance_label.Name = "maintainance_label";
+            this.maintainance_label.Size = new System.Drawing.Size(167, 25);
+            this.maintainance_label.TabIndex = 3;
+            this.maintainance_label.Text = "Maintainance ID";
+            // 
+            // statusOfFlight_label
+            // 
+            this.statusOfFlight_label.AutoSize = true;
+            this.statusOfFlight_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusOfFlight_label.Location = new System.Drawing.Point(124, 283);
+            this.statusOfFlight_label.Name = "statusOfFlight_label";
+            this.statusOfFlight_label.Size = new System.Drawing.Size(166, 25);
+            this.statusOfFlight_label.TabIndex = 4;
+            this.statusOfFlight_label.Text = "Status Of Flight ";
+            // 
+            // revenueGenerated_label
+            // 
+            this.revenueGenerated_label.AutoSize = true;
+            this.revenueGenerated_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.revenueGenerated_label.Location = new System.Drawing.Point(124, 220);
+            this.revenueGenerated_label.Name = "revenueGenerated_label";
+            this.revenueGenerated_label.Size = new System.Drawing.Size(205, 25);
+            this.revenueGenerated_label.TabIndex = 5;
+            this.revenueGenerated_label.Text = "Revenue Generated";
+            // 
+            // cost_label
+            // 
+            this.cost_label.AutoSize = true;
+            this.cost_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cost_label.Location = new System.Drawing.Point(125, 337);
+            this.cost_label.Name = "cost_label";
+            this.cost_label.Size = new System.Drawing.Size(56, 25);
+            this.cost_label.TabIndex = 6;
+            this.cost_label.Text = "Cost";
+            // 
+            // airline_comboBox
+            // 
+            this.airline_comboBox.FormattingEnabled = true;
+            this.airline_comboBox.Location = new System.Drawing.Point(396, 48);
+            this.airline_comboBox.Name = "airline_comboBox";
+            this.airline_comboBox.Size = new System.Drawing.Size(187, 21);
+            this.airline_comboBox.TabIndex = 7;
+            // 
+            // maintainance_comboBox
+            // 
+            this.maintainance_comboBox.FormattingEnabled = true;
+            this.maintainance_comboBox.Location = new System.Drawing.Point(396, 165);
+            this.maintainance_comboBox.Name = "maintainance_comboBox";
+            this.maintainance_comboBox.Size = new System.Drawing.Size(187, 21);
+            this.maintainance_comboBox.TabIndex = 8;
+            // 
+            // flightid_comboBox
+            // 
+            this.flightid_comboBox.FormattingEnabled = true;
+            this.flightid_comboBox.Location = new System.Drawing.Point(396, 105);
+            this.flightid_comboBox.Name = "flightid_comboBox";
+            this.flightid_comboBox.Size = new System.Drawing.Size(187, 21);
+            this.flightid_comboBox.TabIndex = 9;
+            // 
+            // revenueGeneratedOut_label
+            // 
+            this.revenueGeneratedOut_label.AutoSize = true;
+            this.revenueGeneratedOut_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.revenueGeneratedOut_label.Location = new System.Drawing.Point(442, 220);
+            this.revenueGeneratedOut_label.Name = "revenueGeneratedOut_label";
+            this.revenueGeneratedOut_label.Size = new System.Drawing.Size(56, 25);
+            this.revenueGeneratedOut_label.TabIndex = 10;
+            this.revenueGeneratedOut_label.Text = "Cost";
+            this.revenueGeneratedOut_label.Click += new System.EventHandler(this.revenueGeneratedOut_label_Click);
+            // 
+            // StatusOfFlightOut_label
+            // 
+            this.StatusOfFlightOut_label.AutoSize = true;
+            this.StatusOfFlightOut_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusOfFlightOut_label.Location = new System.Drawing.Point(442, 283);
+            this.StatusOfFlightOut_label.Name = "StatusOfFlightOut_label";
+            this.StatusOfFlightOut_label.Size = new System.Drawing.Size(56, 25);
+            this.StatusOfFlightOut_label.TabIndex = 11;
+            this.StatusOfFlightOut_label.Text = "Cost";
+            this.StatusOfFlightOut_label.Click += new System.EventHandler(this.StatusOfFlightOut_label_Click);
+            // 
+            // costOut_label
+            // 
+            this.costOut_label.AutoSize = true;
+            this.costOut_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.costOut_label.Location = new System.Drawing.Point(442, 337);
+            this.costOut_label.Name = "costOut_label";
+            this.costOut_label.Size = new System.Drawing.Size(56, 25);
+            this.costOut_label.TabIndex = 12;
+            this.costOut_label.Text = "Cost";
+            this.costOut_label.Click += new System.EventHandler(this.costOut_label_Click);
+            // 
+            // AS_revenue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.costOut_label);
+            this.Controls.Add(this.StatusOfFlightOut_label);
+            this.Controls.Add(this.revenueGeneratedOut_label);
+            this.Controls.Add(this.flightid_comboBox);
+            this.Controls.Add(this.maintainance_comboBox);
+            this.Controls.Add(this.airline_comboBox);
+            this.Controls.Add(this.cost_label);
+            this.Controls.Add(this.revenueGenerated_label);
+            this.Controls.Add(this.statusOfFlight_label);
+            this.Controls.Add(this.maintainance_label);
+            this.Controls.Add(this.flightID_label);
+            this.Controls.Add(this.aircraftId_label);
+            this.Name = "AS_revenue";
+            this.Text = "AS_revenue";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AS_revenue_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label aircraftId_label;
+        private System.Windows.Forms.Label flightID_label;
+        private System.Windows.Forms.Label maintainance_label;
+        private System.Windows.Forms.Label statusOfFlight_label;
+        private System.Windows.Forms.Label revenueGenerated_label;
+        private System.Windows.Forms.Label cost_label;
+        private System.Windows.Forms.ComboBox airline_comboBox;
+        private System.Windows.Forms.ComboBox maintainance_comboBox;
+        private System.Windows.Forms.ComboBox flightid_comboBox;
+        private System.Windows.Forms.Label revenueGeneratedOut_label;
+        private System.Windows.Forms.Label StatusOfFlightOut_label;
+        private System.Windows.Forms.Label costOut_label;
+    }
+}
---
+++ AS_revenue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DATABASE_PROJECT
+{
+    public partial class AS_revenue : Form
+    {
+        public AS_revenue()
+        {
+            InitializeComponent();
+        }
+
+        private void AS_revenue_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void revenueGeneratedOut_label_Click(object sender, EventArgs e)
+        {
+            // total revenue generated through bookings 
+        }
+
+        private void StatusOfFlightOut_label_Click(object sender, EventArgs e)
+        {
+            // either in profit or loss 
+            // total revenue generated - cost
+        }
+
+        private void costOut_label_Click(object sender, EventArgs e)
+        {
+            // cost of flight 
+        }
+    }
+}
